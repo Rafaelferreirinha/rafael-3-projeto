250ea6a9407469d03c17f43ba4d27e2de525268c
Rafaelferreirinha <162988754+Rafaelferreirinha@users.noreply.github.com>
2024-11-04 09:40:13 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,5 +16,11 @@ cartao.innerHTML = `
 </div>
 </div>
 `
+let respostaEstaVisivel = false
+function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
 container.appendChild(cartao)
 }
